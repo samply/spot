563b6bcf1b2d22f30a346fbe3b450b04d39dbc5f
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-10-10 07:21:28 +0000

    Send lens query as a raw string

--- src/beam.rs
+++ src/beam.rs
@@ -1,11 +1,11 @@
-use beam_lib::{TaskRequest, AppId};
+use beam_lib::{TaskRequest, AppId, RawString};
 use crate::CONFIG;
 
 pub fn create_beam_task(
     id: beam_lib::MsgId,
     target_sites: Vec<String>,
     query: String,
-) -> TaskRequest<String> {
+) -> TaskRequest<RawString> {
     let proxy_id = CONFIG.beam_app.proxy_id();
     let broker_id = proxy_id.as_ref().split_once('.').expect("Invalid beam id in config").1;
     let to = target_sites.into_iter().map(|site| AppId::new_unchecked(format!("focus.{site}.{broker_id}"))).collect();
@@ -14,7 +14,7 @@ pub fn create_beam_task(
         from: CONFIG.beam_app.clone(),
         to,
         metadata: serde_json::Value::Null,
-        body: query,
+        body: query.into(),
         failure_strategy: beam_lib::FailureStrategy::Retry {
             backoff_millisecs: 1000,
             max_tries: 5,
