15c9351f8592f1d50ded7f9da69a234691e2fd22
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-11-13 08:36:14 +0000

    Add focus obfuscation support

--- src/beam.rs
+++ src/beam.rs
@@ -9,11 +9,18 @@ pub fn create_beam_task(
     let proxy_id = CONFIG.beam_app_id.proxy_id();
     let broker_id = proxy_id.as_ref().split_once('.').expect("Invalid beam id in config").1;
     let to = target_sites.into_iter().map(|site| AppId::new_unchecked(format!("focus.{site}.{broker_id}"))).collect();
+    let metadata = if let Some(project) = &CONFIG.project {
+        serde_json::json!({
+            "project": project
+        })
+    } else {
+        serde_json::Value::Null
+    };
     TaskRequest {
         id,
         from: CONFIG.beam_app_id.clone(),
         to,
-        metadata: serde_json::Value::Null,
+        metadata,
         body: query.into(),
         failure_strategy: beam_lib::FailureStrategy::Retry {
             backoff_millisecs: 1000,
