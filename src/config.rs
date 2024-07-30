cf3b8ee188d69d09a5764788ec6a09e287b3f881
janskiba <jan.skiba@dkfz-heidelberg.de>
2024-07-30 08:38:41 +0000

    A new local spot

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,5 @@
-use std::{convert::Infallible, net::SocketAddr, path::PathBuf};
+use std::net::SocketAddr;
 
-use beam_lib::AppId;
 use clap::Parser;
 use reqwest::{Url, header::InvalidHeaderValue};
 use tower_http::cors::AllowOrigin;
@@ -8,41 +7,17 @@ use tower_http::cors::AllowOrigin;
 #[derive(Parser, Clone, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
-    /// URL of the Beam Proxy
+    /// Url of the local blaze server including /fhir/
     #[clap(long, env)]
-    pub beam_proxy_url: Url,
-
-    /// Beam AppId of this application
-    #[clap(long, env, value_parser = |v: &str| Ok::<_, Infallible>(AppId::new_unchecked(v)))]
-    pub beam_app_id: AppId,
-
-    /// Credentials to use on the Beam Proxy
-    #[clap(long, env)]
-    pub beam_secret: String,
+    pub blaze_url: Url,
 
     /// Where to allow cross-origin resourse sharing from
     #[clap(long, env, value_parser = parse_cors)]
     pub cors_origin: AllowOrigin,
 
-    /// Optional project name used by focus
-    #[clap(long, env)]
-    pub project: Option<String>,
-
     /// The socket address this server will bind to
     #[clap(long, env, default_value = "0.0.0.0:8055")]
     pub bind_addr: SocketAddr,
-
-    /// URL to catalogue.json file
-    #[clap(long, env)]
-    pub catalogue_url: Option<Url>,
-
-    /// URL to prism
-    #[clap(long, env, default_value= "http://localhost:8066")]
-    pub prism_url: Url,
-
-    /// Path to a file which will contain the query logs
-    #[clap(long, env, value_hint = clap::ValueHint::FilePath)]
-    pub log_file: Option<PathBuf>,
 }
 
 fn parse_cors(v: &str) -> Result<AllowOrigin, InvalidHeaderValue> {
--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,39 @@ use axum::{
     routing::{get, post},
     Router
 };
-use std::{collections::HashMap, io, path::PathBuf, sync::Arc, time::{SystemTime, UNIX_EPOCH}};
+use mini_moka::sync::Cache;
+use std::{convert::Infallible, sync::Arc, time::Duration};
 
-use beam::create_beam_task;
-use beam_lib::{BeamClient, MsgId, TaskResult};
+use beam_lib::{AppId, MsgId, RawString, TaskResult};
 use clap::Parser;
 use config::Config;
 use once_cell::sync::Lazy;
-use reqwest::{header, Method, StatusCode};
+use reqwest::{header, Client, Method, StatusCode};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tower_http::cors::CorsLayer;
 use tracing::{info, warn, Level};
 use tracing_subscriber::{util::SubscriberInitExt, EnvFilter};
-use tokio::{io::AsyncWriteExt, sync::{mpsc, Mutex}};
-use futures_util::{TryFutureExt, TryStreamExt};
+use tokio::{net::TcpListener, time::Instant};
 use health::{BeamStatus, HealthOutput, Verdict};
 
 mod banner;
-mod beam;
-mod catalogue;
 mod config;
 mod health;
 
 static CONFIG: Lazy<Config> = Lazy::new(Config::parse);
 
-static BEAM_CLIENT: Lazy<BeamClient> = Lazy::new(|| {
-    BeamClient::new(
-        &CONFIG.beam_app_id,
-        &CONFIG.beam_secret,
-        CONFIG.beam_proxy_url.clone(),
-    )
-});
+static BLAZE_CLIENT: Lazy<Client> = Lazy::new(|| Client::builder().default_headers(HeaderMap::from_iter([(header::CONTENT_TYPE, HeaderValue::from_static("application/json"))])).build().unwrap());
 
-type ResultLogSenderMap = Arc<Mutex<HashMap<MsgId, mpsc::Sender<String>>>>;
-
-#[derive(Clone, Default)]
+#[derive(Clone)]
 struct SharedState {
-    extended_json: Arc<Mutex<Value>>,
-    result_log_sender_map: ResultLogSenderMap,
+    results: Cache<MsgId, Arc<TaskResult<RawString>>>
+}
+
+impl SharedState {
+    fn new() -> Self {
+        Self { results: Cache::builder().time_to_live(Duration::from_secs(2 * 60)).build() }
+    }
 }
 
 #[tokio::main]
@@ -53,13 +47,6 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .finish()
         .init();
-
-    // TODO: Remove this workaround once clap manages to not choke on URL "".
-    if let Ok(var) = std::env::var("CATALOGUE_URL") {
-        if var.is_empty() {
-            std::env::remove_var("CATALOGUE_URL");
-        }
-    }
 
     info!("{:#?}", Lazy::force(&CONFIG));
 
@@ -73,23 +60,14 @@ async fn main() {
         .route("/beam", post(handle_create_beam_task))
         .route("/beam/:task_id", get(handle_listen_to_beam_tasks));
 
-    let state = if let Some(url) = CONFIG.catalogue_url.clone() {
-        let extended_json = catalogue::spawn_thing(url, CONFIG.prism_url.clone());
-        app = app.route("/catalogue", get(handle_get_catalogue));
-        SharedState { extended_json, result_log_sender_map: Default::default() }
-    } else {
-        SharedState::default()
-    };
-    let app = app.with_state(state)
+    let app = app
         .layer(axum::middleware::map_response(banner::set_server_header))
-        .layer(cors);
-
-    // TODO: Add check for reachability of beam-proxy
+        .layer(cors)
+        .with_state(SharedState::new());
 
     banner::print_banner();
 
-    axum::Server::bind(&CONFIG.bind_addr)
-        .serve(app.into_make_service())
+    axum::serve(TcpListener::bind(&CONFIG.bind_addr).await.unwrap(), app)
         .await
         .unwrap();
 }
@@ -109,19 +87,67 @@ async fn handler_health() -> Json<HealthOutput> {
 }
 
 async fn handle_create_beam_task(
-    State(SharedState { result_log_sender_map, .. }): State<SharedState>,
-    headers: HeaderMap,
+    State(SharedState { results }): State<SharedState>,
     Json(query): Json<LensQuery>,
-) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
-    if let Some(log_file) = &CONFIG.log_file {
-        tokio::spawn(log_query(log_file, query.clone(), headers, result_log_sender_map));
+) -> Result<impl IntoResponse, StatusCode> {
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct CqlQuery {
+        pub lib: Value,
+        pub measure: Value
     }
-    let LensQuery { id, sites, query } = query;
-    let task = create_beam_task(id, sites, query);
-    BEAM_CLIENT.post_task(&task).await.map_err(|e| {
-        warn!("Unable to query Beam.Proxy: {}", e);
-        (StatusCode::BAD_GATEWAY, "Unable to query Beam.Proxy")
+    let LensQuery { id, query, .. } = query;
+    let query: CqlQuery = serde_json::from_str(&query).map_err(|_| StatusCode::BAD_REQUEST)?;
+    let measure_url = query.measure["url"].as_str().ok_or_else(|| {
+        warn!("Measure did not contain valid url: {:#?}", query.measure);
+        StatusCode::BAD_REQUEST
     })?;
+    BLAZE_CLIENT
+        .post(format!("{}Library", CONFIG.blaze_url))
+        .body(query.lib.to_string())
+        .send()
+        .await
+        .map_err(|e| {
+            warn!("Blaze req failed: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .error_for_status()
+        .map_err(|e| {
+            warn!("Blaze unexpected status: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+    BLAZE_CLIENT
+        .post(format!("{}Measure", CONFIG.blaze_url))
+        .header("Content-Type", "application/json")
+        .body(query.measure.to_string())
+        .send()
+        .await
+        .map_err(|e| {
+            warn!("Blaze req failed: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .error_for_status()
+        .map_err(|e| {
+            warn!("Blaze unexpected status: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+    let result = BLAZE_CLIENT.get(format!("{}Measure/$evaluate-measure?measure={}&periodStart=2000&periodEnd=2030", CONFIG.blaze_url, measure_url))
+        .send()
+        .await
+        .map_err(|e| {
+            warn!("Blaze req failed: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .error_for_status()
+        .map_err(|e| {
+            warn!("Blaze unexpected status: {e:#}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .text()
+        .await
+        .map_err(|_| {
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+    results.insert(id, Arc::new(TaskResult { from: AppId::new_unchecked("local.local.broker"), to: vec![AppId::new_unchecked("local.local.broker")], task: id, status: beam_lib::WorkStatus::Succeeded, body: RawString(result), metadata: Value::Null }));
     Ok(StatusCode::CREATED)
 }
 
@@ -132,100 +158,18 @@ struct ListenQueryParameters {
 
 async fn handle_listen_to_beam_tasks(
     Path(task_id): Path<MsgId>,
-    Query(listen_query_parameter): Query<ListenQueryParameters>,
-    State(SharedState { result_log_sender_map, .. }): State<SharedState>
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let resp = BEAM_CLIENT
-        .raw_beam_request(
-            Method::GET,
-            &format!(
-                "v1/tasks/{}/results?wait_count={}",
-                task_id, listen_query_parameter.wait_count
-            ),
-        )
-        .header(
-            header::ACCEPT,
-            HeaderValue::from_static("text/event-stream"),
-        )
-        .send()
-        .await
-        .map_err(|err| {
-            println!(
-                "Failed request to {} with error: {}",
-                CONFIG.beam_proxy_url, err
-            );
-            (
-                StatusCode::BAD_GATEWAY,
-                "Error calling beam, check the server logs.".to_owned(),
-            )
-        })?;
-    let code = resp.status();
-    if !code.is_success() {
-        return Err((code, resp.text().await.unwrap_or_else(|e| e.to_string())));
-    }
-    let sender =  result_log_sender_map.lock().await.remove(&task_id);
-    if sender.is_none() && CONFIG.log_file.is_some() {
-        warn!("Logging is enabled but no log sender found for logging results.");
-    }
-    let stream = async_sse::decode(resp.bytes_stream().map_err(io::Error::other).into_async_read())
-        .and_then(move |event| {
-            let sender = sender.clone();
-            async move { match event {
-                async_sse::Event::Retry(_) => unreachable!("Beam does not send retries!"),
-                async_sse::Event::Message(m) => {
-                    if let Ok(result) = serde_json::from_slice::<TaskResult<beam_lib::RawString>>(m.data()) {
-                        if result.status == beam_lib::WorkStatus::Succeeded {
-                            if let Some(sender) = sender {
-                                sender.send(result.from.as_ref().split('.').nth(1).unwrap().to_owned()).await.expect("not dropped");
-                            }
-                        }
-                    }
-                    Ok(Event::default().data(String::from_utf8_lossy(m.data())).event(m.name()))
-                },
-            }
-        }});
-    Ok(Sse::new(stream))
-}
-
-async fn log_query(log_file: &PathBuf, query: LensQuery, headers: HeaderMap, result_logger_map: ResultLogSenderMap) {
-    #[derive(Serialize)]
-    struct Log {
-        user_email: String,
-        #[serde(flatten)]
-        query: LensQuery,
-        ts: u128,
-        results: Vec<String>
-    }
-    let (tx, mut rx) = mpsc::channel(query.sites.len());
-    result_logger_map.lock().await.insert(query.id, tx);
-    let mut results = Vec::with_capacity(query.sites.len());
-    while let Some(result) = rx.recv().await {
-        results.push(result);
-    }
-    let user_email = headers
-        .get("x-auth-request-email")
-        .unwrap_or(&HeaderValue::from_static("Unknown user"))
-        .to_str()
-        .expect("Should be a valid string")
-        .to_owned();
-    let mut out = serde_json::to_vec(&Log {
-        user_email,
-        query,
-        results,
-        ts: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
-    }).expect("Failed to serialize log");
-    out.push(b'\n');
-    let res = tokio::fs::OpenOptions::new()
-        .append(true)
-        .open(log_file)
-        .and_then(|mut f| async move { f.write(&out).await })
-        .await;
-    if let Err(e) = res {
-        warn!("Failed to write to log file: {e}");
+    Query(_listen_query_parameter): Query<ListenQueryParameters>,
+    State(SharedState { results }): State<SharedState>
+) -> Result<impl IntoResponse, StatusCode> {
+    // TODO: Send the result via a channel instead
+    let in_30s = Instant::now() + Duration::from_secs(30);
+    let result = loop {
+        match results.get(&task_id) {
+            Some(res) => break res,
+            None if !(Instant::now() > in_30s) => tokio::time::sleep(Duration::from_millis(250)).await,
+            None => return Err(StatusCode::NO_CONTENT)
+        }
     };
-}
-
-async fn handle_get_catalogue(State(state): State<SharedState>) -> Json<Value> {
-    // TODO: We can totally avoid this clone by using axum_extra ErasedJson
-    Json(state.extended_json.lock().await.clone())
+    let event = Event::default().data(serde_json::to_string(result.as_ref()).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?);
+    Ok(Sse::new(futures_util::stream::once(async { Ok::<_, Infallible>(event) })))
 }
